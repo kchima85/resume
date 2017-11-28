bff40a314a4cf5c45536d14ebabd1615348048c7
Karmbir Chima <karmbirchima@gmail.com>
2017-11-27 23:22:59 -0800

    added waypoints

--- js/js.js
+++ js/js.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
   $('.second-pic-wp').waypoint(function(direction) {
     $('.second-pic-wp').addClass('animated fadeInLeft')
   }, {
-    offset: '597px'
+    offset: '596px'
   })
 
 });
