0d6c96d579a748da2e50b142da1baaafeefe28b7
Karmbir Chima <karmbirchima@gmail.com>
2017-11-28 23:04:35 -0800

    added more styling

--- js/js.js
+++ js/js.js
@@ -81,14 +81,14 @@ $(document).ready(function() {
     $('.project-1').addClass('animated fadeInLeft')
     $('.project-2').addClass('animated fadeInRight')
   }, {
-    offset: '1000px'
+    offset: '900px'
   })
 
   $('.interests-hobbies').waypoint(function(direction) {
     $('.interests-hobbies').addClass('animated zoomIn')
     $('.hobbies').addClass('animated fadeIn')
   }, {
-    offset: '1000px'
+    offset: '900px'
   })
   
 });
