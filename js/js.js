8c4c836f4d007f45a7da2040123da8bb464f4953
Karmbir Chima <karmbirchima@gmail.com>
2017-11-30 22:24:57 -0800

    conditionally removing carousel

--- js/js.js
+++ js/js.js
@@ -1,77 +1,80 @@
 $(document).ready(function() {
-  $('.main-carousel').flickity({
-    // options
-    cellAlign: 'center',
-    contain: true,
-    pageDots: false
-  });
+  if (window.matchMedia("(min-width: 992px)").matches) {
+    console.log('in the if');
+    animations();
+  } else {
+    console.log('in the else');
+    $('div').removeClass('main-carousel')
+    animations();
+  }
 
-  $('.about-me').waypoint(function(direction) {
-    $('.second-pic-wp').addClass('animated fadeInLeft')
-    $('.about-me-text').addClass('animated fadeInRight')
-  }, {
-    offset: '50%'
-  })
-
-  $('.buttons').waypoint(function(direction) {
-    $('.buttons').addClass('animated fadeInUp')
-  }, {
-    offset: '50%'
-  })
-
-  $('.skill-set').waypoint(function(direction) {
-    $('.skill-section').addClass('animated zoomIn')
-  }, {
-    offset: '50%'
-  })
-
-  $('.tech-skills-1').waypoint(function(direction) {
-    $('.tech-skills-1').addClass('animated slideInUp')
-  }, {
-    offset: '50%'
-  })
-
-  $('.tech-skills-2').waypoint(function(direction) {
-    $('.tech-skills-2').addClass('animated slideInUp')
-  }, {
-    offset: '50%'
-  })
-
-  $('.tech-skills-3').waypoint(function(direction) {
-    $('.tech-skills-3').addClass('animated slideInUp')
-  }, {
-    offset: '50%'
-  })
-
-  $('.education-section').waypoint(function(direction) {
-    var waypoint = new Waypoint ({
-      element: document.getElementById('first-carousel'),
-      handler: function(direction) {
-
-      },
-      context: document.getElementById('first-carousel')
+  function animations() {
+    $('.main-carousel').flickity({
+      // options
+      cellAlign: 'center',
+      contain: true,
+      pageDots: false
+    });
+  
+    $('.about-me').waypoint(function(direction) {
+      $('.second-pic-wp').addClass('animated fadeInLeft')
+      $('.about-me-text').addClass('animated fadeInRight')
+    }, {
+      offset: '50%'
     })
-    waypoint.context.refresh()
-    $('.education-section').addClass('animated zoomIn')
-  }, {
-    offset: '90%'
-  })
-
-  $('.colleges').waypoint(function(direction) {
-    $('.colleges').addClass('animated fadeIn')
-  }, {
-    offset: '90%'
-  })
-
-  $('.projects-header').waypoint(function(direction) {
-    $('.projects-header').addClass('animated zoomIn')
-  }, {
-    offset: '90%'
-  })
-
-  $('.projects').waypoint(function(direction) {
-    $('.projects').addClass('animated fadeIn')
-  }, {
-    offset: '90%'
-  })
+  
+    $('.buttons').waypoint(function(direction) {
+      $('.buttons').addClass('animated fadeInUp')
+    }, {
+      offset: '50%'
+    })
+  
+    $('.skill-set').waypoint(function(direction) {
+      $('.skill-section').addClass('animated zoomIn')
+    }, {
+      offset: '50%'
+    })
+  
+    $('.tech-skills-1').waypoint(function(direction) {
+      $('.tech-skills-1').addClass('animated slideInUp')
+    }, {
+      offset: '50%'
+    })
+  
+    $('.tech-skills-2').waypoint(function(direction) {
+      $('.tech-skills-2').addClass('animated slideInUp')
+    }, {
+      offset: '50%'
+    })
+  
+    $('.tech-skills-3').waypoint(function(direction) {
+      $('.tech-skills-3').addClass('animated slideInUp')
+    }, {
+      offset: '50%'
+    })
+  
+    $('.education-section').waypoint(function(direction) {
+      $('.education-section').addClass('animated zoomIn')
+    }, {
+      offset: '80%'
+    })
+  
+    $('.colleges').waypoint(function(direction) {
+      $('.colleges').addClass('animated fadeIn')
+    }, {
+      offset: '80%'
+    })
+  
+    $('.projects-header').waypoint(function(direction) {
+      $('.projects-header').addClass('animated zoomIn')
+    }, {
+      offset: '80%'
+    })
+  
+    $('.projects').waypoint(function(direction) {
+      $('.projects').addClass('animated fadeIn')
+    }, {
+      offset: '80%'
+    })
+  }
 });
